87b0ad7c624fdfab330d0e0fed843130c0c32bdc
luoli <luoli523@gmail.com>
2019-03-27 10:24:41 +0800

    update the submit script to make it more transplatable

--- submit.sh
+++ submit.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 home="${HOME}"
-spark_home="${SPARK_HOME}"
-submit_script="${spark_home}/bin/spark-submit"
 
-#this="${BASH_SOURCE-$0}"
-#common_bin=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
-#script="$(basename -- "$this")"
-#this="$common_bin/$script"
+if [ -z ${SPARK_HOME} ]; then
+  echo "Shoud set the \$SPARK_HOME"
+  exit 1
+fi
 
-submit_dir="${HOME}/dev/git/luoli/MySbtScalaProject"
+submit_script="${SPARK_HOME}/bin/spark-submit"
+
+this="${BASH_SOURCE-$0}"
+submit_dir=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
+#submit_dir="${HOME}/dev/git/luoli/MySbtScalaProject"
 submit_jar="simple-spark_2.11-1.0.jar"
 
 clz=$1
